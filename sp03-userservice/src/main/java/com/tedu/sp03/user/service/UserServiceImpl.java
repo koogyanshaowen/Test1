b72ea87ba4fb472629e81f5dc815cce159bcc7ec
Administrator <Administrator@tarena>
2019-08-28 09:30:25 +0800

    更新配置

--- sp03-userservice/src/main/java/com/tedu/sp03/user/service/UserServiceImpl.java
+++ sp03-userservice/src/main/java/com/tedu/sp03/user/service/UserServiceImpl.java
@@ -1,7 +1,9 @@
 package com.tedu.sp03.user.service;
 import java.util.List;
 
+import org.hibernate.validator.constraints.Range;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Service;
 
 import com.fasterxml.jackson.core.type.TypeReference;
@@ -11,6 +13,7 @@ import com.tedu.web.util.JsonUtil;
 
 import lombok.extern.slf4j.Slf4j;
 //数据从配置文件中获取的；
+@RefreshScope
 @Slf4j
 @Service
 public class UserServiceImpl implements UserService {
